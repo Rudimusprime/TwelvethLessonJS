c8f5d56fb164301e6e36f0431cfea2ef0d02c127
DanielB <d.banasiewicz@pdo.com.pl>
2021-09-28 21:26:54 +0200

    ToDo List on array

--- FourthExample/scripts/main.js
+++ FourthExample/scripts/main.js
@@ -1,3 +1,5 @@
+const toDoList = [];
+
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
@@ -6,7 +8,10 @@ const input = document.querySelector('input');
 
 const removeTask = (e) => {
     e.target.parentNode.remove();
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1);
     taskNumber.textContent = listItems.length;
+    renderList();
 }
 
 const addTask = (e) => {
@@ -16,9 +21,18 @@ const addTask = (e) => {
     const task = document.createElement('li');
     task.className = 'task';
     task.innerHTML = titleTask + "<button>Usuń</button>";
+    toDoList.push(task);
+    renderList();
     ul.appendChild(task);
     input.value = "";
     taskNumber.textContent = listItems.length;
     task.querySelector('button').addEventListener('click', removeTask);
 }
-form.addEventListener('submit', addTask)
+const renderList = () => {
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement);
+    })
+}
+form.addEventListener('submit', addTask);
