0f7d45a22b031d7656ad15a2333ded9b84ac8829
DanielB <d.banasiewicz@pdo.com.pl>
2021-09-28 17:26:35 +0200

    removeTask function is done

---
+++ FirstExample/scripts/main.js
@@ -0,0 +1,9 @@
+const removeTask = (e) => {
+    // e.target.remove();
+    // e.target.parentNode.remove();
+    // e.target.parentNode.style.textDecoration = "line-through";
+    // e.target.remove();
+    const index = e.target.dataset.key;
+    document.querySelector(`[data-key="${index}"]`).remove();
+}
+document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
